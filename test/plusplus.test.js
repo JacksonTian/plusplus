54cffbe9ab1a8a68e1aec536a81983ed3e993edd
Jackson Tian <shyvo1987@gmail.com>
2012-08-10 02:04:32 +0800

    Add testcases.

---
+++ test/plusplus.test.js
@@ -0,0 +1,21 @@
+var plus = require('../lib/plusplus');
+var should = require('should');
+
+describe("PlusPlus", function () {
+  
+  it("Tablify", function () {
+    var list = [
+        {"username": "JacksonTian", "nick": "朴灵", "hometown": "Chongqing"},
+        {"username": "Fengmk2", "nick": "苏千", "hometown": "Guangzhou"}
+    ];
+    var ret = plus.tablify(list);
+    var expect = [
+     ["username", "nick", "hometown"],
+     ["JacksonTian", "朴灵", "Chongqing"],
+     ["Fengmk2", "苏千", "Guangzhou"]
+    ];
+    ret.should.eql(expect);
+  });
+
+  // TODO
+});
